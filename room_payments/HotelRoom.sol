c70582cd584a54067e256512d16fefc163f54f33
kartik1998 <kartikrawat61@gmail.com>
2021-09-13 01:15:50 +0530

    Add: reciever

--- room_payments/HotelRoom.sol
+++ room_payments/HotelRoom.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.0;
 contract HotelRoom {
     address payable public owner;
     enum Statuses { Vacant, Occupied }
+    event Occupy(address _occupant, uint _value);
     Statuses currentStatus;
     
     constructor() public {
@@ -16,10 +17,14 @@ contract HotelRoom {
         _;
     }
     
-    function book() public payable onlyWhileVacant {
-        // Check price
-        require(msg.value >= 2 ether, "not enough ether provided");
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "not enough ether provided");
+        _;
+    }
+    
+    receive() external payable onlyWhileVacant costs(2 ether){
         currentStatus = Statuses.Occupied;
         owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
     }
 }
