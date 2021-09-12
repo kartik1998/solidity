0baca9924f1c52d2ada9edd9de9ddfc6af71ce43
kartik1998 <kartikrawat61@gmail.com>
2021-09-12 23:37:19 +0530

    Add: conditionals

---
+++ Conditionals.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.6.0; 
+
+contract Conditionals {
+    uint[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    
+    function isEvenNumber(uint _number) public view returns(bool) {
+        if(_number % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    function countEvenNumbers() public view returns(uint) {
+        uint count = 0;
+        for(uint i = 0; i < numbers.length; i++) {
+            if(isEvenNumber(numbers[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
