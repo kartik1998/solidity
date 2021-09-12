b607fc90b117b0aa57fe5964ff2d56a9cea97501
kartik1998 <kartikrawat61@gmail.com>
2021-09-12 23:31:31 +0530

    Add: nested mapping

--- Mappings.sol
+++ Mappings.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.6.0;
 contract Mappings {
     mapping(uint => string) public names;
     mapping(uint => Book) public books;
+    mapping(address => mapping(uint => Book)) public myBooks;
     
     struct Book {
         string title;
@@ -17,5 +18,9 @@ contract Mappings {
     
     function addBook(uint _id, string memory _title, string memory _author) public {
         books[_id] = Book(_title, _author);
+    }
+    
+    function addMyBook(uint _id, string memory _title, string memory _author) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
     }
 }
